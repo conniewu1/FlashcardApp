276dc5546621b0b42d132b17d85a3fd28bce5a74
Connie Wu <cw342@duke.edu>
2018-10-13 18:29:06 -0400

    Optional tasks added

--- app/src/main/java/com/example/conniewuflashcard/flashcardapp/MainActivity.java
+++ app/src/main/java/com/example/conniewuflashcard/flashcardapp/MainActivity.java
@@ -18,6 +18,47 @@ public class MainActivity extends AppCompatActivity {
                 findViewById(R.id.flashcard_question).setVisibility(View.INVISIBLE);
             }
         });
+
+        findViewById(R.id.flashcard_answer).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                findViewById(R.id.flashcard_answer).setVisibility(View.INVISIBLE);
+                findViewById(R.id.flashcard_question).setVisibility(View.VISIBLE);
+            }
+        });
+
+        findViewById(R.id.option1).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                findViewById(R.id.option1).setBackgroundColor(getResources().getColor(R.color.my_red_color, null));
+                findViewById(R.id.option3).setBackgroundColor(getResources().getColor(R.color.my_green_color, null));
+            }
+        });
+
+        findViewById(R.id.option2).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                findViewById(R.id.option2).setBackgroundColor(getResources().getColor(R.color.my_red_color, null));
+                findViewById(R.id.option3).setBackgroundColor(getResources().getColor(R.color.my_green_color, null));
+            }
+        });
+
+        findViewById(R.id.option3).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                findViewById(R.id.option3).setBackgroundColor(getResources().getColor(R.color.my_green_color, null));
+            }
+        });
+
+        findViewById(R.id.rootView).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                findViewById(R.id.option1).setBackgroundColor(getResources().getColor(R.color.my_pink_color, null));
+                findViewById(R.id.option2).setBackgroundColor(getResources().getColor(R.color.my_pink_color, null));
+                findViewById(R.id.option3).setBackgroundColor(getResources().getColor(R.color.my_pink_color, null));
+            }
+        });
+
     }
 
 }
